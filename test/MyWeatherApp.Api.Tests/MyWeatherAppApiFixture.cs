6d7d80f78e33c777bb9f493e64f26252878540eb
joseftw <josef@localhost.local>
2022-10-19 14:41:22 +0200

    Adds config to fixture

--- test/MyWeatherApp.Api.Tests/MyWeatherAppApiFixture.cs
+++ test/MyWeatherApp.Api.Tests/MyWeatherAppApiFixture.cs
@@ -1,5 +1,20 @@
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.Configuration;
 
 namespace MyWeatherApp.Api.Tests;
 
-public class MyWeatherAppApiFixture : WebApplicationFactory<Program> {}
+public class MyWeatherAppApiFixture : WebApplicationFactory<Program>
+{
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+        var testConfiguration = new MyWeatherAppApiTestConfiguration();
+        builder
+            .UseEnvironment(testConfiguration["ASPNETCORE_ENVIRONMENT"])
+            .ConfigureAppConfiguration(configurationBuilder =>
+            {
+                configurationBuilder.AddInMemoryCollection(testConfiguration!);
+            });
+    }
+}
